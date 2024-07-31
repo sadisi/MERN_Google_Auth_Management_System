8aacc5072b625414366fd1cd701c0aa1eb532bf4
Vilochana Rajapaksha <sadisi.rajapaksha218346@gmail.com>
2024-07-31 18:32:02 +0530

    server set

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
-
-const app = express();
 const PORT = process.env.PORT || "8090";
 
 
+const app = express();
 app.use(cors());
 app.use(express.json({limit: '20mb'}));
 
-app.listen(PORT)
+app.get("/", (req, res) => {
+    res.send("<h2>Welcome to Library Management System API</h2>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port : ${PORT}`);
+});
 
